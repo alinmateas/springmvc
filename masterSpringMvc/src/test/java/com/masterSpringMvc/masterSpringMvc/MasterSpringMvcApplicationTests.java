191920aa4c699194e0b779406cb6538cf82dc236
alinmateas <alin_10messi@yahoo.com>
2020-02-22 23:04:48 +0200

    generated with start.spring.io

---
+++ masterSpringMvc/src/test/java/com/masterSpringMvc/masterSpringMvc/MasterSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterSpringMvc.masterSpringMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
